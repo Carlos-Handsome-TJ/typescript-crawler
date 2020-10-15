bb2bea9f315d8fa385ee3e1d4aefc5cb86deb8bd
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2020-10-15 15:42:46 +0800

    爬取基本信息

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,16 +1,34 @@
 import superagent from 'superagent'
+import cheerio from 'cheerio'
 
+interface Course {
+    title: string
+}
 class Crawler {
-    // private searchKey = 'crowllerKey'
-    // private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.searchKey}`
-    private url = 'http://localhost:3000/'
-    private getRawHtml = ''
-    async getHtml() {
+    private key = '"x3b174jsx'
+    private url = 'http://www.dell-lee.com/typescript/demo.html?secret=' + this.key
+    getCourseInfo(html: string) {
+        const $ = cheerio.load(html)
+        const courseItems = $('.course-item')
+        const courseData: Course[] = []
+        courseItems.map((index, item) => {
+            const desc = $(item).find('.course-desc').text()
+            courseData.push({
+                title: desc,
+            })
+        })
+        const courseInfo = {
+            time: (new Date()).getTime(),
+            data: courseData
+        }
+        console.log(courseInfo)
+    }
+    async getRawHtml() {
         const result = await superagent.get(this.url)
-        console.log(result.text)
-        // console.log('1231')
+        this.getCourseInfo(result.text)
     }
     constructor() {
-        this.getHtml()
+        this.getRawHtml()
     }
 }
+const crawler = new Crawler()
