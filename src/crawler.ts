f493b5c3cb65a3e52664feb38bb84188af63a597
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2020-10-16 15:24:28 +0800

    爬取网页内容，将json文件存储在本地文件夹

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,34 +1,68 @@
+import fs from 'fs'
+import path from 'path'
 import superagent from 'superagent'
 import cheerio from 'cheerio'
 
 interface Course {
-    title: string
+    title: string;
+    count: number;
+}
+interface FileContent {
+    [propName: number]: Course[];
+}
+interface CourseInfo {
+    time: number;
+    data: Course[];
 }
 class Crawler {
-    private key = '"x3b174jsx'
+    private key = 'http://www.dell-lee.com/typescript/demo.html?secret='
     private url = 'http://www.dell-lee.com/typescript/demo.html?secret=' + this.key
-    getCourseInfo(html: string) {
-        const $ = cheerio.load(html)
-        const courseItems = $('.course-item')
-        const courseData: Course[] = []
-        courseItems.map((index, item) => {
-            const desc = $(item).find('.course-desc').text()
-            courseData.push({
-                title: desc,
-            })
-        })
-        const courseInfo = {
-            time: (new Date()).getTime(),
-            data: courseData
-        }
-        console.log(courseInfo)
-    }
+    private filePath = path.resolve(__dirname, '../data/data.json')
+    //获取HTML文件：
     async getRawHtml() {
         const result = await superagent.get(this.url)
-        this.getCourseInfo(result.text)
+        return result.text
+    }
+    //从HTML文件中获取course信息：
+    getCourseInfo(html: string) {
+        const $ = cheerio.load(html)
+        const courseItem = $('.course-item')
+        const courseInfo: Course[] = []
+        courseItem.map((index, item) => {
+            const title = $(item).find('.course-desc').eq(0).text()
+            const count = parseInt($(item).find('.course-desc').eq(1).text().split('：')[1], 10)
+            courseInfo.push({
+                title,
+                count
+            })
+        })
+        return {
+            time: (new Date()).getTime(),
+            data: courseInfo
+        }
+    }
+    //把获取到的数据转化成JSON文件格式：
+    getJsonContent(courseResult: CourseInfo) {
+        let courseContent: FileContent = {}
+        if (fs.existsSync(this.filePath)) {
+            courseContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+        }
+        courseContent[courseResult.time] = courseResult.data
+        return JSON.stringify(courseContent)
+    }
+    //将文件写入到本地文件夹当中：
+    writeFile(courseContent: string) {
+        fs.writeFileSync(this.filePath, courseContent)
+    }
+    //处理爬取功能函数：
+    async initSpiderProcess() {
+        const html = await this.getRawHtml()
+        const courseResult = this.getCourseInfo(html)
+        const fileContent = this.getJsonContent(courseResult)
+        this.writeFile(fileContent)
     }
     constructor() {
-        this.getRawHtml()
+        this.initSpiderProcess()
     }
 }
-const crawler = new Crawler()
+new Crawler()
