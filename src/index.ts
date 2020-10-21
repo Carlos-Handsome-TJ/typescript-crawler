be16a9694dce4b37be7b82ca98ab78c6bf5df1bf
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2020-10-21 15:23:25 +0800

    完成登录功能的基本开发

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
 import router from './router'
 
 const app = express()
+//解析post表单中的内容：
 app.use(bodyParser.urlencoded({ extended: false }))
+//登录中间件：
+app.use(cookieSession({
+    name: 'session',
+    keys: ['carlos'],
+    maxAge: 1000 * 60 * 60 * 24
+}))
+//后端路由处理：
 app.use(router)
 app.listen(3000, () => {
-    console.log('success send')
+    console.log('后端运行正常~')
 })
--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,80 @@
 import { Router, Request, Response } from 'express'
+import fs from 'fs'
+import path from 'path'
 import Crawler from './crawler'
 import Analyzer from './analyzer'
 
+interface BodyRequest extends Request {
+    body: {
+        [key: string]: string | undefined;
+    }
+}
 const router = Router()
 router.get('/', (req, res) => {
-    res.send(`
-    <html>
-        <body>
-            <form method="post" action="/getData">
-                <input type="password" name="password"/>
-                <button type="submit">提交</button>
-            </form>
-        </body>
-    </html>
+    const isLogin = req.session ? req.session.login : undefined
+    if (isLogin) {
+        res.send(`
+            <html>
+                <body>
+                    <a href="/logout">退出登录</a>
+                </body>
+            </html>
+        `)
+    } else {
+        res.send(`
+        <html>
+            <body>
+                <form method="post" action="/login">
+                    <input type="password" name="password" />
+                    <button type="submit">点击登录</button>
+                </form>
+            </body>
+        </html>
     `)
+    }
 })
-router.post('/getData', (req: Request, res: Response) => {
-    console.log(req.body.password)
-    // const url = 'http://www.dell-lee.com/typescript/demo.html?secret' + 'x3b174jsx'
-    // const analyze = Analyzer.getInstance()
-    // new Crawler(url, analyze)
-    res.send('getData success')
+//登录：
+router.post('/login', (req: BodyRequest, res: Response) => {
+    const { password } = req.body
+    const isLogin = req.session ? req.session.login : undefined
+    if (isLogin) {
+        res.send('已经登录~')
+    } else {
+        if (password === '123' && req.session) {
+            req.session.login = true
+            res.send(`
+                <html>
+                    <body>
+                        <a href="/getData">爬取数据</a>
+                        <a href="/logout">退出登录</a>
+                    </body>
+                </html>
+            `)
+        } else {
+            res.send('密码不正确~')
+        }
+    }
 })
+//退出登录：
+router.get('/logout', (req: Request, res: Response) => {
+    const isLogin = req.session ? req.session.login : undefined
+    if (isLogin && req.session) {
+        req.session.login = undefined
+    }
+    res.redirect('/')
+})
+//爬取数据：
+router.get('/getData', (req: Request, res: Response) => {
+    const url = 'http://www.dell-lee.com/typescript/demo.html?secret' + 'x3b174jsx'
+    const analyze = Analyzer.getInstance()
+    new Crawler(url, analyze)
+    try {
+        const fileContent = fs.readFileSync(path.resolve(__dirname, '../data/data.json'), 'utf-8')
+        res.json(JSON.parse(fileContent))
+    } catch(e) {
+        console.log(e)
+        res.send('尚未爬取到内容~')
+    }
+})
+
 export default router
